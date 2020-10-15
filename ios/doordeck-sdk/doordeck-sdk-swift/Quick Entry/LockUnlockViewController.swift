2c0458799a0bf1c7210b168ca254460f51fed266
Rafa Ruiz <raruizmu@gmail.com>
2020-10-15 15:31:44 +0100

    Update to the latest versions of Android and iOS (#10)
    * Update the Android version
    
    * Update readme
    
    * Update hard copy of Swift library
    
    * Update README.md
    
    * Revert package name
    
    * Update to latest Swift SDK
    
    * Get ready for next
    
    Co-authored-by: marwan87 <marwan87@users.noreply.github.com>

--- ios/doordeck-sdk/doordeck-sdk-swift/Quick Entry/LockUnlockViewController.swift
+++ ios/doordeck-sdk/doordeck-sdk-swift/Quick Entry/LockUnlockViewController.swift
@@ -43,7 +43,7 @@ class LockUnlockViewController: UIViewController {
         self.sodium = sodiumTemp
         self.certificateChain = chain
         self.lockVariable = lockVariableTemp
-        super.init(nibName: nil, bundle: nil)
+        super.init(nibName: nil, bundle: Bundle(for: type(of: self)))
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -170,7 +170,7 @@ class LockUnlockViewController: UIViewController {
 
 extension LockUnlockViewController {
     private func showWaitScreen (_ delay: Double) {
-        loadingView.addDelayTimer(delay - 1)
+        loadingView.addDelayTimer(delay)
     }
     
     private func showLoadingScreen () {
