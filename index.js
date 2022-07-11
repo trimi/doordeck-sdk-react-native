2e0a7cb86da00bd6775aa65d593d468c342b661f
Leutrim Rexhepi <leutrim@thecommons.club>
2022-07-12 01:36:22 +0200

    showunlock argument

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import { NativeModules } from 'react-native';
+import { NativeModules } from "react-native";
 
 const { RNDoordeckSdk } = NativeModules;
 
 const logout = () => RNDoordeckSdk.logout();
-const initDoordeck = (authToken, darkMode = true, closeButton = false) => RNDoordeckSdk.initDoordeck(authToken, darkMode, closeButton);
-const showUnlock = () => RNDoordeckSdk.showUnlock();
+const initDoordeck = (authToken, darkMode = true, closeButton = false) =>
+  RNDoordeckSdk.initDoordeck(authToken, darkMode, closeButton);
+const showUnlock = (isNfc) => RNDoordeckSdk.showUnlock(isNfc);
 
 module.exports = {
-    logout,
-    initDoordeck,
-    showUnlock
-}
+  logout,
+  initDoordeck,
+  showUnlock,
+};
